a5ce2c3f724fd5d816f2f269ec2cda9e259e3ce7
Syed Mahmood Aleem Huzaifa <55015582+smahuzaifa@users.noreply.github.com>
2024-05-02 09:50:25 +1000

    Jenkins file
    the file has pipeline script to be used

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,78 @@
+pipeline{
+    agent any
+
+    stages{
+        stage('Build'){
+            steps{
+                echo 'Building the code using a build automation tool which will compile and package the code'
+                echo 'The tool used is Maven'
+            }
+        }
+        stage('Unit and Integration Testing'){
+            steps{
+                echo 'Unit testing is done to ensure the code works as per the expectations'
+                echo 'Integration testing is also done to ensure multiple components of the applications are wroking as per the expected behaviour'
+                echo 'The tool used for unit testing is JUnit'
+                echo 'The tool used for integration testing is Appium'
+            }
+            post{
+                success {
+                    mail to: 'mention email',
+                    subject: 'Unit and Integration Testing Status: Success',
+                    body: 'The unit and integration testing was done successfully',
+                    attachLog: true
+                }
+                failure{
+                    mail to: 'mention email',
+                    subject: 'Unit and Integration Testing Status: Failed',
+                    body: 'The unit and integration testing failed. Please check the logs for details',
+                    attachLog: true
+                }
+            }
+        }
+        stage('Code Analysis'){
+            steps{
+                echo 'The code is analysed to ensure it is written as per the industry standard'
+                echo 'The tool used is SonarQube'
+            }
+        }
+        stage('Security Scan'){
+            steps{
+                echo 'A security scan is performed to identify any vulnerabilities'
+                echo 'The tool used is OWASP ZAP'
+            }
+            post{
+                success {
+                    mail to: 'mention email',
+                    subject: 'Security scan - SUCCESS',
+                    body: 'Security scan was conducted successfully',
+                    attachLog: true
+                }
+                failure{
+                    mail to: 'mention email',
+                    subject: 'Security scan - FAILED',
+                    body: 'Security scan failed',
+                    attachLog: true
+                }
+            }
+        }
+        stage('Deploy to Staging'){
+            steps{
+                echo 'The application is being deployed to the staging server'
+                echo 'The tool used is Docker with AWS EC2'
+            }
+        }
+        stage('Integration testing on staging'){
+            steps{
+                echo 'Integration testing is done on the staging environment'
+                echo 'The tool used is Postman'
+            }
+        }
+        stage('Deploy to production'){
+            steps{
+                echo 'The application is being deployed to the production server'
+                echo 'The tool used is Kubernetes with AWS EC2'
+            }
+        }
+    }
+}
